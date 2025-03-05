8dec0e5351a47930796a4732f7cd7ebdbaedbd61
Asjer Bereket <asjer.bereket@uke.oslo.kommune.no>
2025-03-05 15:35:42 +0100

    small design changes

--- public/Dashboard/Dashboard.js
+++ public/Dashboard/Dashboard.js
@@ -8,27 +8,22 @@ fetch("/getLoggedInUser")
   .catch((error) => console.error("Feil ved henting av bruker:", error));
 
 function openTab(evt, tabName) {
-  // Declare variables
   var i, tabcontent, tablinks;
 
-  // Hide all tab content
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the "active" class from all tab buttons
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab and add an "active" class to the button
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
-// Open the first tab by default
 document.addEventListener("DOMContentLoaded", function () {
   const firstTab = document.getElementsByClassName("tablinks")[0];
   if (firstTab) {
@@ -110,7 +105,7 @@ async function handleLogout() {
         const data = await response.json();
         
         if (data.status) {
-            window.location.href = '/login'; // Omdirigerer til login-siden
+            window.location.href = '/login'; 
         } else {
             console.error('Kunne ikke logge ut');
         }
