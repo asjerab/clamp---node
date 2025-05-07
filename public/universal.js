724ee1167e3f4b6baf3bcffd1035d3fd3200c24b
asjerab <asjer.contact@gmail.com>
2025-05-07 16:02:29 +0200

    jobber videre på mobile navigation

--- public/universal.js
+++ public/universal.js
@@ -5,26 +5,27 @@
 const links = [
   {
     name: "Home",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-      <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-    </svg>
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+</svg>
+
     `,
-    href: "/index.html",
+    href: "/",
   },
   {
     name: "About",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-  </svg>
-  `,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+</svg>
+`,
     href: "/About/index.html",
   },
   {
     name: "Projects",
-    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-  </svg>
-  `,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+</svg>
+`,
     href: "/Project/index.html",
   },
 ];
@@ -33,29 +34,8 @@ const navigationMenu = document.getElementById("NavigationMenu");
 navigationMenu.innerHTML = links
   .map(
     (link) => `
-    <a class="Gilroy-Semibold flex justify-between gap-5 items-center text-[22px] flex-1 py-2 hover:scale-101 duration-200 group active:scale-99" style="font-size: clamp(15px, 10vw, 20px)" href="${link.href}">
-        <p>${link.name}</p>
-        ${link.icon}
-    </a>
-`
-  )
-  .join("");
-
-// Kode for footer menu lenker
-const footerLinks = [
-  { name: "About", href: "#" },
-  { name: "Work", href: "#" },
-  { name: "Contact", href: "#" },
-  { name: "Terms of Service", href: "#" },
-  { name: "Privacy Policy", href: "#" },
-];
-
-const footerMenu = document.getElementById("footerMenu");
-footerMenu.innerHTML = footerLinks
-  .map(
-    (link) => `
-    <a class="hover:-translate-y-2 hover:underline duration-150 ease-in-out" href="${link.href}">
-        <p class="Gilroy-Medium text-[18px]">${link.name}</p>
+    <a href="${link.href}" class="flex items-center justify-between">
+<p class="Gilroy-Semibold my-1" style="font-size: clamp(22px, 10vw, 35px)">${link.name}</p>        ${link.icon}
     </a>
 `
   )
